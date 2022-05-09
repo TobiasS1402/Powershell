20ed468f4a28353ad2af7a2b32c91be2cec84567
TobiasS1402 <tobias.seijsener@student.hu.nl>
2022-05-09 18:16:01 +0200

    Update scripts for for valid certificate chain and auto renew for ADFS.

--- importPfsenseCertChain.ps1
+++ importPfsenseCertChain.ps1
@@ -1,15 +1,12 @@
 <#
   .SYNOPSIS
-  Tobias, 6-8-2021.
-  This script is based on 2 articles i have read about downloading ACME certificates via the Pfsense webUI. In this specific case i tailored the script specifically for the use on my Windows Admin Center host.
-
+  Tobias, 9-5-2022.
+  This script is based on 2 articles i have read about downloading ACME certificates via the Pfsense webUI.
   .DESCRIPTION
-  This script downloads ACME certificates from Pfsense and imports them into your WAC instance.
-
+  This script downloads ACME certificates from Pfsense and imports them into certificate manager with the CA certificate inside
   .EXAMPLE
-  PS> .\PfsenseACME-WacSSL.ps1
-  Scheduled task: powershell.exe -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File c:\PfsenseACME-WacSSL.ps1
-
+  PS> .\importPfsenseCertChain.ps1
+  Scheduled task: powershell.exe -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File c:\importPfsenseCertChain.ps1
   .LINK
   Webrequests to Pfsense based on:
   https://www.chadmccune.net/2020/07/30/scripting-pfsense-dhcp-static-assignments/
@@ -19,12 +16,11 @@
 $PFSENSE_USERNAME = "CertMgmt" #Username of your restricted useraccoun
 $PFSENSE_PASSWORD = "Password" #Password of your restricted useraccount
 $SITE = "https://fqdn.pfsense.local" #FQDN or IP address of your Pfsense instance
-$CERTID = "certid" #Pfsense certificate id to download
-$CERTNAME = "intranet" #Name to give to the downloaded files
-$CERTDIR = "c:\certificates\intranet" #Location on your Windows instance where the certificates will be downloaded
-$PFXPASSWORD = "pfxpassword" #password to generate and import the pfx file with certutil
-$GUID = New-Guid | Select-Object -ExpandProperty Guid #$GUID generates a GUID for the netsh app id
-$WACPORT = 443 #Port on which your WAC is running
+$CERTID = "" #Pfsense certificate id to download
+$CAID = "" #Pfsense CA certificate id to download
+$CERTNAME = "" #Name to give to the downloaded files
+$CERTDIR = "c:\windows\temp" #Location on your Windows instance where the certificates will be downloaded
+$PFXPASSWORD = "" #password to generate and import the pfx file with certutil
 
 try {
     try {
@@ -49,6 +45,10 @@ try {
     $postParams = @{act='exp';id=$CERTID;__csrf_magic=$lastRequest.InputFields[0].value}
     (Invoke-WebRequest "$SITE/system_certmanager.php" -OutFile "$CERTDIR\$CERTNAME.crt" -WebSession $pfSenseSession -Method Post -Body $postParams -UseBasicParsing)
 
+    #Request to download the CA certificate
+    $postParams = @{act='exp';id=$CAID;__csrf_magic=$lastRequest.InputFields[0].value}
+    (Invoke-WebRequest "$SITE/system_camanager.php" -OutFile "$CERTDIR\ca.crt" -WebSession $pfSenseSession -Method Post -Body $postParams -UseBasicParsing)
+
     #Request to download the private key
     $postParams = @{act='key';id=$CERTID;__csrf_magic=$lastRequest.InputFields[0].value}
     (Invoke-WebRequest "$SITE/system_certmanager.php" -OutFile "$CERTDIR\$CERTNAME.key" -WebSession $pfSenseSession -Method Post -Body $postParams)
@@ -58,16 +58,11 @@ catch {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 }
 
-#merge pfx for Windows Admin Center and import the certificate into the personal LocalMachine store
-certutil.exe -p "$PFXPASSWORD,$PFXPASSWORD" -f -MergePFX $CERTDIR\$CERTNAME.crt $CERTDIR\$CERTNAME.pfx
-$thumbprint = Import-pfxCertificate -FilePath "$CERTDIR\$CERTNAME.pfx" -password (ConvertTo-SecureString $PFXPASSWORD -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My | Select-Object -ExpandProperty Thumbprint
+#merge pfx and import the certificate into the personal LocalMachine store
+cat $CERTDIR\"ca.crt" >> $CERTDIR\$CERTNAME.crt #merge cert with ca cert
+certutil.exe -p "$PFXPASSWORD,$PFXPASSWORD" -f -MergePFX $CERTDIR\$CERTNAME.crt $CERTDIR\$CERTNAME.pfx #Merge the PFX itself
+$thumbprint = Import-pfxCertificate -FilePath "$CERTDIR\$CERTNAME.pfx" -password (ConvertTo-SecureString $PFXPASSWORD -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My | Select-Object -ExpandProperty Thumbprint #select thumbprint
 
 #Cleanup actions for the certificates
-Remove-Item -Path $CERTDIR\*.* -Force
+Remove-Item  -Exclude "$CERTNAME.pfx" -Path $CERTDIR\*.* -Force
 
-#netsh remove ssl config for WAC @ port 443
-netsh http delete sslcert ipport=0.0.0.0:$WACPORT
-netsh http add sslcert ipport=0.0.0.0:$WACPORT certhash=$thumbprint appid="{$GUID}"
-
-#restart the WAC service for sslconfig to take effect
-Restart-Service ServerManagementGateway -Force
---
+++ importPfsenseCertChainADFS.ps1
@@ -0,0 +1,74 @@
+<#
+  .SYNOPSIS
+  Tobias, 9-5-2022.
+  This script is based on 2 articles i have read about downloading ACME certificates via the Pfsense webUI.
+  .DESCRIPTION
+  This script downloads ACME certificates from Pfsense and imports them into certificate manager with the CA certificate inside
+  .EXAMPLE
+  PS> .\importPfsenseCertChain.ps1
+  Scheduled task: powershell.exe -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File c:\importPfsenseCertChain.ps1
+  .LINK
+  Webrequests to Pfsense based on:
+  https://www.chadmccune.net/2020/07/30/scripting-pfsense-dhcp-static-assignments/
+  https://forum.netgate.com/topic/123405/get-certificates-from-pfsense-cert-manager-using-linux-commandline/4
+#>
+
+$PFSENSE_USERNAME = "CertMgmt" #Username of your restricted useraccoun
+$PFSENSE_PASSWORD = "Password" #Password of your restricted useraccount
+$SITE = "https://fqdn.pfsense.local" #FQDN or IP address of your Pfsense instance
+$CERTID = "" #Pfsense certificate id to download
+$CAID = "" #Pfsense CA certificate id to download
+$CERTNAME = "" #Name to give to the downloaded files
+$CERTDIR = "c:\windows\temp" #Location on your Windows instance where the certificates will be downloaded
+$PFXPASSWORD = "" #password to generate and import the pfx file with certutil
+
+try {
+    try {
+        if ((Test-Path -Path $CERTDIR -ErrorAction Stop) -eq $false){
+            New-Item -ItemType Directory -Path $CERTDIR
+        }
+        else {
+        }
+    }
+    catch {
+        New-Item -ItemType Directory -Path $CERTDIR
+    }
+
+    #Request to get the CSRF token for this session
+    $lastRequest = (Invoke-WebRequest "$SITE/system_certmanager.php" -SessionVariable pfSenseSession -UseBasicParsing)
+    
+    #Request to authenticate
+    $postParams = @{login='Login';usernamefld=$PFSENSE_USERNAME;passwordfld=$PFSENSE_PASSWORD;__csrf_magic=$lastRequest.InputFields[0].value}
+    $lastRequest = (Invoke-WebRequest "$SITE/system_certmanager.php" -WebSession $pfSenseSession -Method Post -Body $postParams -UseBasicParsing)
+
+    #Request to download the certificate
+    $postParams = @{act='exp';id=$CERTID;__csrf_magic=$lastRequest.InputFields[0].value}
+    (Invoke-WebRequest "$SITE/system_certmanager.php" -OutFile "$CERTDIR\$CERTNAME.crt" -WebSession $pfSenseSession -Method Post -Body $postParams -UseBasicParsing)
+
+    #Request to download the CA certificate
+    $postParams = @{act='exp';id=$CAID;__csrf_magic=$lastRequest.InputFields[0].value}
+    (Invoke-WebRequest "$SITE/system_camanager.php" -OutFile "$CERTDIR\ca.crt" -WebSession $pfSenseSession -Method Post -Body $postParams -UseBasicParsing)
+
+    #Request to download the private key
+    $postParams = @{act='key';id=$CERTID;__csrf_magic=$lastRequest.InputFields[0].value}
+    (Invoke-WebRequest "$SITE/system_certmanager.php" -OutFile "$CERTDIR\$CERTNAME.key" -WebSession $pfSenseSession -Method Post -Body $postParams)
+}
+catch {
+    #TLS config as system / service acount
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+
+#merge pfx for and import the certificate into the personal LocalMachine store
+cat $CERTDIR\"ca.crt" >> $CERTDIR\$CERTNAME.crt #merge cert with ca cert
+certutil.exe -p "$PFXPASSWORD,$PFXPASSWORD" -f -MergePFX $CERTDIR\$CERTNAME.crt $CERTDIR\$CERTNAME.pfx #Merge the PFX itself
+$thumbprint = Import-pfxCertificate -FilePath "$CERTDIR\$CERTNAME.pfx" -password (ConvertTo-SecureString $PFXPASSWORD -AsPlainText -Force) -CertStoreLocation Cert:\LocalMachine\My | Select-Object -ExpandProperty Thumbprint #select thumbprint for further action
+
+#ADFS commands for renewing the certificate and reloading the services
+Set-AdfsCertificate -CertificateType Service-Communications -Thumbprint $thumbprint #import certificate into ADFS
+Update-AdfsCertificate -Urgent #Urgently update the ADFS certificate
+Restart-Service adfssrv #Restart the ADFS service
+Set-AdfsSslCertificate -Thumbprint $thumbprint #Set the right Certificate thumbprint for the ADFS certificate
+
+#Cleanup actions for the certificates
+Remove-Item -Exclude "$CERTNAME.pfx" -Path $CERTDIR\*.* -Force
+
